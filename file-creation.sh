6d2a018372839b85f8b9bc5282d71e8e9e2d69ee
mallika <21nn1a0513.mallika@gmail.com>
2026-06-10 22:45:52 +0530

    roles structure

---
+++ file-creation.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+folders=$(find roles -type d | awk -F"/" '{print $2}')
+
+for i in ${folders[@]}
+do 
+  echo "folder $i"
+  mkdir -p "roles/$i/files"
+  mkdir -p "roles/$i/templates"
+  mkdir -p "roles/$i/tasks"
+  mkdir -p "roles/$i/vars"
+done 
+
+#move files to respective folders 
+
+for i in ${folders[@]}
+do 
+    d=("yaml" "service" "repo")
+    for j in ${d[@]}
+    do 
+        echo $j 
+        yaml=$(find roles/$i -type f -name "*.$j")
+        if [ -z "$yaml" ]; then 
+            echo "null yaml varibale" 
+        else 
+            file_name=$(basename -- "$yaml")
+            echo $file_name 
+            if [ -e "roles/$i/$file_name" ]; then 
+                if [ "$j" == "service" ]; then 
+                    mv roles/$i/$file_name roles/$i/templates/
+                fi 
+                if [ "$j" == "yaml" ]; then 
+                    mv roles/$i/$file_name roles/$i/tasks/main.yaml 
+                fi 
+                if [ "$j" == "repo" ]; then  
+                    mv roles/$i/$file_name roles/$i/files/
+                fi 
+            fi 
+        fi 
+    done 
+done 
+
+
+#rename all yamls into main.yaml inside tasks folder 
+
